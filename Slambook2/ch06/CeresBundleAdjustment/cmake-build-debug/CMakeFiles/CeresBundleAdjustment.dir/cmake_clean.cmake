9dcdeb779170f16bcef78b90bb95a53d8cf43eac
zhoupengwei <zpwhuat@163.com>
2020-01-20 11:22:57 +0800

    2020/1/20

---
+++ Slambook2/ch06/CeresBundleAdjustment/cmake-build-debug/CMakeFiles/CeresBundleAdjustment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CeresBundleAdjustment.dir/main.cpp.o"
+  "CeresBundleAdjustment"
+  "CeresBundleAdjustment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CeresBundleAdjustment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
